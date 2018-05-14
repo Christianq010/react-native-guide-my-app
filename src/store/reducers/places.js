284dd6bc3c7fa89ecd77c2fc84b26dc21d9625ae
Christiaan Quyn <christianq010@gmail.com>
2018-05-15 01:01:00 +0530

    Add Redux to react native

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -8,8 +8,40 @@ const initialState = {
 // set state to initialState if state is not defined
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        default: 
-            return state;
+    case ADD_PLACE:
+        return {
+        ...state,
+        places: state.places.concat({
+            key: Math.random(),
+            name: action.placeName,
+            image: {
+            uri:
+                "https://cdn.dribbble.com/users/1666570/screenshots/4584965/motherhood_dribbble.jpg"
+            }
+        })
+        };
+    case DELETE_PLACE:
+        return {
+        ...state,
+        places: state.places.filter(place => {
+            return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+        };
+    case SELECT_PLACE:
+        return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+            return place.key === action.placeKey;
+        })
+        };
+    case DESELECT_PLACE:
+        return {
+        ...state,
+        selectedPlace: null
+        };
+    default:
+        return state;
     }
 };
 
